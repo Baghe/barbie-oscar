bd0b81abfc0421ec7f7b7a6fd557ebfd1ff324f4
baghe <baghega1@gmail.com>
2025-02-03 22:17:08 +0100

    v0.4

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/barbie-oscar',
+  base: '/barbie-oscar/',
   resolve: {
     alias: {
       src: "/src",
