b74dd01b445c5b4d0964fb3ef0b2674591b5a04f
baghe <baghega1@gmail.com>
2025-02-06 14:16:31 +0100

    v1.0

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,13 +1,14 @@
 import { retrieveLaunchParams } from '@telegram-apps/sdk-react';
 import { useEffect, useState } from 'react';
 import { ApiCall } from './Functions/ApiCall';
+import ImgLogo from './logo.png';
+import './app.css';
 
 export default function App({ Private }) {
   const { initData: Auth, initDataRaw: AuthRaw } = Private ? {} : retrieveLaunchParams()
   const [App, setApp] = useState(null)
   const [AppTime, setAppTime] = useState(null)
   const [AppOnline, setAppOnline] = useState(false)
-  const [WinnerView, setWinnerView] = useState(false)
   const UpdateOnline = 20
   const USER_ADMIN = Private ? false : 1194709210 === Auth.user.id
 
@@ -60,6 +61,34 @@ export default function App({ Private }) {
     })
   }
 
+  function CategoryClear(IdCategory) {
+    if (window.confirm('Eliminare tutti i voti?')) {
+      ApiCall(AuthRaw, 'category-clear', {
+        Value: {
+          IdCategory: IdCategory,
+        },
+      }, () => {
+        AppUpdate()
+      })
+    }
+  }
+
+  function CandidateToggle(IdCategory, IdUser, Hidden) {
+    ApiCall(AuthRaw, 'candidate-toggle', {
+      Value: {
+        IdCategory: IdCategory,
+        IdUser: IdUser,
+        Hidden: Hidden,
+      },
+    }, () => {
+      AppUpdate()
+    })
+  }
+
+  function Acronym(Name) {
+    return Name.split(' ').map((Word) => Word.slice(0, 1)).join('')
+  }
+
   return (
     <div className="container-fluid py-2">
       {Auth && (
@@ -79,18 +108,26 @@ export default function App({ Private }) {
 
       {App && (
         <>
+          {/* LOGO */}
+          {!App.OpenVote && (
+            <div className="px-5 text-center mb-4">
+              <img src={ImgLogo} alt="Logo" className="img-fluid mx-auto w-100" style={{ maxWidth: 300 }} />
+            </div>
+          )}
+
+          {/* VOTAZIONE */}
           {Auth && (
-            App.OpenVote ? (
+            App.OpenVote && (
               (() => {
                 const Category = App.Categories[App.OpenVote]
                 const Votes = Object.values(Category.Candidates).reduce((Votes, Candidate) => Votes + Candidate.Votes, 0)
                 const MyVote = App.Votes[Category.IdCategory] || null
                 return (
                   <>
-                    <div className="text-center mb-5">
+                    <div className="text-center mb-4 cherry-swash-regular lh-1">
                       <div className="display-4">#{Category.IdCategory}</div>
-                      <div className="text-muted">Per la categoria</div>
-                      <div className="h1 fw-bold">{Category.Name}</div>
+                      <div className="text-muted text-white fs-5">Per la categoria</div>
+                      <div className="display-2 text-center fw-bold text-barbie">{Category.Name}</div>
                       {Votes < App.Voters ? (
                         <div className="text-muted small">
                           Votazione in corso
@@ -99,17 +136,56 @@ export default function App({ Private }) {
                       ) : (
                         <div className="fw-bold text-success">Votazione conclusa</div>
                       )}
+
+                      {USER_ADMIN && (
+                        <button className="btn btn-sm btn-danger" onClick={() => CategoryOpen(-1)}>
+                          Chiudi votazione
+                        </button>
+                      )}
                     </div>
 
-
-                    {WinnerView ? (
-                      <>
-
-                        <div className="d-flex flex-column gap-2 mb-5">
-                          {Object.values(Category.Candidates).sort((a, b) => b.Votes - a.Votes).map((Candidate) => {
+                    {MyVote ? (
+                      (() => {
+                        const Voted = App.Users[MyVote]
+                        return (
+                          <>
+                            <div className="small text-center text-muted">
+                              Hai votato per
+                            </div>
+                            <div className="bg-success rounded-5 mb-5 p-3 text-center">
+                              {Voted.Image ? (
+                                <img src={Voted.Image} alt="Candidate"
+                                  className="rounded-circle" style={{ width: 128, height: 128 }} />
+                              ) : (
+                                <div className="rounded-circle mx-auto bg-light bg-opacity-75" style={{ width: 128, height: 128, lineHeight: "128px" }}>
+                                  <div className="text-center text-dark fw-bold">
+                                    <div className="fs-1">
+                                      {Voted.Name.slice(0, 1)}
+                                    </div>
+                                  </div>
+                                </div>
+                              )}
+                              <div className="fw-bold h2 mb-0">
+                                {Voted.Name}
+                              </div>
+                            </div>
+                          </>
+                        )
+                      })()
+                    ) : (
+                      <div className="mb-5">
+                        <div className="d-flex flex-column gap-2">
+                          {Object.values(Category.Candidates).map((Candidate) => {
+                            const Clicked = VoteForm.IdCategory === Category.IdCategory && VoteForm.IdUser === Candidate.IdUser
                             const User = App.Users[Candidate.IdUser]
+                            const IsMe = Candidate.IdUser === Auth.user.id
                             return (
-                              <div key={Candidate.IdUser} className="d-flex align-items-center rounded-pill border border-3 p-2">
+                              <button key={Candidate.IdUser}
+                                className={"p-2 d-flex align-items-center rounded-pill border-3 cursor-pointer btn " + (Clicked ? "btn-primary" : "btn-outline-primary")}
+                                onClick={() => setVoteForm({
+                                  IdCategory: Category.IdCategory,
+                                  IdUser: Candidate.IdUser,
+                                })} disabled={IsMe}>
                                 {User.Image ? (
                                   <img src={User.Image} alt="Candidate"
                                     className="rounded-circle" style={{ width: 64, height: 64 }} />
@@ -122,7 +198,7 @@ export default function App({ Private }) {
                                     </div>
                                   </div>
                                 )}
-                                <div className="lh-1 px-3 flex-grow-1">
+                                <div className="ms-3 text-start lh-1 pe-2">
                                   <div className="fs-3 mb-0 fw-bold">
                                     {User.Name}
                                   </div>
@@ -132,140 +208,83 @@ export default function App({ Private }) {
                                     </div>
                                   )}
                                 </div>
-                                <div className="text-center pe-3 text-success lh-1">
-                                  <div className="fs-2 fw-bold">{Candidate.Votes}</div>
-                                  <div className="small">Voti</div>
-                                </div>
-                              </div>
+                              </button>
                             )
                           })}
                         </div>
-                      </>
-                    ) : (
-                      MyVote ? (
-                        (() => {
-                          const Voted = App.Users[MyVote]
-                          return (
-                            <>
-                              <div className="small text-center text-muted">
-                                Hai votato per
-                              </div>
-                              <div className="bg-success rounded-5 mb-5 p-3 text-center">
-                                {Voted.Image ? (
-                                  <img src={Voted.Image} alt="Candidate"
-                                    className="rounded-circle" style={{ width: 128, height: 128 }} />
-                                ) : (
-                                  <div className="rounded-circle mx-auto bg-light bg-opacity-75" style={{ width: 128, height: 128, lineHeight: "128px" }}>
-                                    <div className="text-center text-dark fw-bold">
-                                      <div className="fs-1">
-                                        {Voted.Name.slice(0, 1)}
-                                      </div>
-                                    </div>
-                                  </div>
-                                )}
-                                <div className="fw-bold h2 mb-0">
-                                  {Voted.Name}
-                                </div>
-                              </div>
-                            </>
-                          )
-                        })()
-                      ) : (
-                        <div className="mb-5">
-                          <div className="d-flex flex-column gap-2">
-                            {Object.values(Category.Candidates).map((Candidate) => {
-                              const Clicked = VoteForm.IdCategory === Category.IdCategory && VoteForm.IdUser === Candidate.IdUser
-                              const User = App.Users[Candidate.IdUser]
-                              const IsMe = Candidate.IdUser === Auth.user.id
-                              return (
-                                <button key={Candidate.IdUser}
-                                  className={"p-2 d-flex align-items-center rounded-pill border-3 cursor-pointer btn " + (Clicked ? "btn-primary" : "btn-outline-primary")}
-                                  onClick={() => setVoteForm({
-                                    IdCategory: Category.IdCategory,
-                                    IdUser: Candidate.IdUser,
-                                  })} disabled={IsMe}>
-                                  {User.Image ? (
-                                    <img src={User.Image} alt="Candidate"
-                                      className="rounded-circle" style={{ width: 64, height: 64 }} />
-                                  ) : (
-                                    <div className="rounded-circle" style={{ width: 64, height: 64, lineHeight: "64px", backgroundColor: "#f8f9fa" }}>
-                                      <div className="text-center text-dark fw-bold">
-                                        <div className="fs-1">
-                                          {User.Name.slice(0, 1)}
-                                        </div>
-                                      </div>
-                                    </div>
-                                  )}
-                                  <div className="ms-3 text-start lh-1 pe-2">
-                                    <div className="fs-3 mb-0 fw-bold">
-                                      {User.Name}
-                                    </div>
-                                    {Candidate.Note && (
-                                      <div className="text-light mt-1 small">
-                                        {Candidate.Note}
-                                      </div>
-                                    )}
-                                  </div>
-                                </button>
-                              )
-                            })}
-                          </div>
 
-                          <button className="btn btn-lg btn-success rounded-pill w-100 mt-2 py-3 fs-2 fw-bold"
-                            disabled={!VoteForm.IdCategory || !VoteForm.IdUser}
-                            onClick={() => Vote(VoteForm.IdCategory, VoteForm.IdUser)}>
-                            Vota
-                          </button>
-                        </div>
-                      )
+                        <button className="btn btn-lg btn-success rounded-pill w-100 mt-2 py-3 fs-2 fw-bold"
+                          disabled={!VoteForm.IdCategory || !VoteForm.IdUser}
+                          onClick={() => Vote(VoteForm.IdCategory, VoteForm.IdUser)}>
+                          Vota
+                        </button>
+                      </div>
                     )}
                   </>
                 )
               })()
-            ) : (
-              <div className="text-center mb-5 py-5">
-                <div className="display-4">Nessuna votazione aperta</div>
-                <div className="text-muted">Stay tuned</div>
-              </div>
             )
           )}
 
-
-          <div className="display-6 text-center mt-5">Categorie</div>
-          <div className="card bg-secondary bg-opacity-25 rounded-5 overflow-hidden mt-2">
-            {USER_ADMIN && (
-              <div className="card-header p-3 text-center fs-5 fw-bold">
-                <button className="btn btn-sm btn-danger" onClick={() => CategoryOpen(-1)}>
-                  Chiudi
-                </button>
-                <button className="btn btn-sm btn-primary ms-2" onClick={() => setWinnerView(!WinnerView)}>
-                  Winners
-                </button>
-              </div>
-            )}
-            <div className="card-body">
-              {Object.values(App.Categories).map((Category, i, a) => (
-                <div key={Category.IdCategory} className={"py-2" + (i < a.length - 1 ? " border-bottom" : "")}>
-                  <div className="d-flex align-items-center">
+          {/* CATEGORIE */}
+          <div className="display-2 text-center mt-5 mb-4 text-barbie cherry-swash-regular">Categorie</div>
+          <div className="d-flex flex-column gap-3 pb-4">
+            {Object.values(App.Categories).map((Category, i, a) => (
+              <div className="card bg-secondary bg-opacity-25 rounded-4" key={Category.IdCategory}>
+                <div className="card-body px-2 pb-2">
+                  <div className="text-center lh-1 mb-3">
                     {App.OpenVote === Category.IdCategory && (
-                      <span className="badge bg-success rounded-pill me-2">Votazione</span>
+                      <span className="badge bg-success rounded-pill mb-2">Votazione</span>
                     )}
-                    <span className="fs-3">#{Category.IdCategory} {Category.Name}</span>
+                    <div className="display-6 cherry-swash-regular text-white">
+                      #{Category.IdCategory} {Category.Name}
+                    </div>
                   </div>
-                  <div className="lh-1">
+
+                  <div className="row g-2 justify-content-center">
                     {Object.values(Category.Candidates).sort((a, b) => {
                       return USER_ADMIN ? b.Votes - a.Votes : a.IdUser - b.IdUser
                     }).map((Candidate) => {
                       const User = App.Users[Candidate.IdUser]
                       return (
-                        <div key={Candidate.IdUser} className="d-flex align-items-between">
-                          {USER_ADMIN && (
-                            <div className="text-end pe-2 text-danger" style={{ width: 32 }}>
-                              {Candidate.Votes}
+                        <div key={Candidate.IdUser} className="col-sm-6 col-md-4 col-lg-2">
+                          <div className={"d-flex flex-column align-items-center text-center p-2 rounded-3 bg-dark shadow h-100" + (Candidate.Hidden ? " opacity-50" : "")}>
+
+                            {USER_ADMIN && (
+                              <div className="w-100 mb-2 bg-secondary bg-opacity-25 p-2 rounded d-flex justify-content-between align-items-center">
+                                <div>
+                                  {Candidate.Hidden ? (
+                                    <button className="btn btn-sm btn-primary" onClick={() => CandidateToggle(Category.IdCategory, Candidate.IdUser, 0)}>
+                                      Abilita
+                                    </button>
+                                  ) : (
+                                    <button className="btn btn-sm btn-primary" onClick={() => CandidateToggle(Category.IdCategory, Candidate.IdUser, 1)}>
+                                      Disabilita
+                                    </button>
+                                  )}
+
+                                </div>
+                                <div className="text-danger text-nowrap fw-bold">
+                                  {Candidate.Votes} voti
+                                </div>
+                              </div>
+                            )}
+                            <div>
+                              {User.Image ? (
+                                <img src={User.Image} alt="Candidate"
+                                  className="rounded-circle" style={{ width: 48, height: 48 }} />
+                              ) : (
+                                <div className="rounded-circle" style={{ width: 48, height: 48, lineHeight: "48px", backgroundColor: "#f8f9fa" }}>
+                                  <div className="text-center text-dark fw-bold">
+                                    <div className="small">
+                                      {Acronym(User.Name)}
+                                    </div>
+                                  </div>
+                                </div>
+                              )}
                             </div>
-                          )}
-                          <div className="flex-grow-1">
-                            &bull; <b>{User.Name}</b>{Candidate.Note && <small className="ms-1 text-muted">({Candidate.Note})</small>}
+                            <div className="fw-bold">{User.Name}</div>
+                            {Candidate.Note && <div className="small text-muted lh-1">({Candidate.Note})</div>}
                           </div>
                         </div>
                       )
@@ -274,17 +293,22 @@ export default function App({ Private }) {
 
                   {USER_ADMIN && (
                     <div className="mt-2 bg-secondary bg-opacity-25 p-2 rounded d-flex justify-content-between align-items-center">
-                      <button className="btn btn-sm btn-danger" onClick={() => CategoryOpen(Category.IdCategory)}>
-                        Apri
-                      </button>
+                      <div className="flex-grow-1 d-flex gap-2">
+                        <button className="btn btn-sm btn-primary" onClick={() => CategoryOpen(Category.IdCategory)}>
+                          Apri votazione
+                        </button>
+                        <button className="btn btn-sm btn-danger" onClick={() => CategoryClear(Category.IdCategory)}>
+                          Cancella voti
+                        </button>
+                      </div>
                       <div>
                         {Object.values(Category.Candidates).reduce((Votes, Candidate) => Votes + Candidate.Votes, 0)}/{App.Voters}
                       </div>
                     </div>
                   )}
                 </div>
-              ))}
-            </div>
+              </div>
+            ))}
           </div>
         </>
       )}
